0057fcbfd4c2d29c3bd38dfdc6708f80d09571fc
seyyaw <seidymam@gmail.com>
2020-06-14 19:36:05 +0200

    e

--- bot/tweet_annotator.py
+++ bot/tweet_annotator.py
@@ -518,7 +518,9 @@ def error(update, context):
             """Log Errors caused by Updates."""
             logger.warning('Update "%s" caused error "%s"', update, context.error)
             message = 'እባክዎ እንደገና ይሞክሩ, /start የሚለውን ንይሞክሩ!'
+            print(message)
             query = update.callback_query
+            print(message,"Again")
             query.edit_message_text(text=message)
         except:
             print('Worst error! No idea')
